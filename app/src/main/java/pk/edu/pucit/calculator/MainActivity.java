7c2610f351a2a4f1444c4e6eaf37c375c61fd463
Saad Imtiaz <58306023+SaadImtiaz786@users.noreply.github.com>
2020-03-21 13:03:42 +0500

    Correct the id name's error and Assign a function to AC

--- app/src/main/java/pk/edu/pucit/calculator/MainActivity.java
+++ app/src/main/java/pk/edu/pucit/calculator/MainActivity.java
@@ -2,6 +2,8 @@ package pk.edu.pucit.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void btn_AC(View view)
+    {
+        TextView newView = findViewById(R.id.tv_result);
+        newView.setText("0");
     }
 }
